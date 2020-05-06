3ea438c42bbe024c90da937fea82a0cd7a7a4abf
mlev71 <max.adam.levinson@gmail.com>
2020-05-06 07:27:46 -0400

    Added structured logging to mongo for all errors and successes on each operation

--- pkg/identifier/mongo.go
+++ pkg/identifier/mongo.go
@@ -9,9 +9,14 @@ import (
 	"encoding/json"
 	"reflect"
 	"fmt"
-	"log"
+
+	"github.com/rs/zerolog"
+	"os"
 )
 
+var (
+	mongoLogger = zerolog.New(os.Stderr).With().Timestamp().Str("backend", "mongo").Logger()
+)
 
 type MongoServer struct {
 	URI      string
@@ -43,11 +48,33 @@ func (ms MongoServer) InsertOne(record interface{}) (err error) {
 	defer cancel()
 
 	if err != nil {
+		// log error for failing to connect
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "InsertOne").
+			Msg("failed connection to mongo")
+
 		return
 	}
 
 	col := client.Database(ms.Database).Collection(ms.Collection)
 	_, err = col.InsertOne(mongoCtx, record)
+
+	if err != nil {
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "InsertOne").
+			Interface("record", record).
+			Msg("failed insert one operation to mongo")
+
+		return
+
+	}
+
+		mongoLogger.Info().
+			Str("operation", "InsertOne").
+			Interface("record", record).
+			Msg("created record in mongo")
 
 	return
 }
@@ -59,6 +86,12 @@ func (ms MongoServer) FindOne(query bson.D) (record []byte, err error) {
 	defer cancel()
 
 	if err != nil {
+		// log error for failing to connect
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "FindOne").
+			Msg("failed connection to mongo")
+
 		return
 	}
 
@@ -66,7 +99,24 @@ func (ms MongoServer) FindOne(query bson.D) (record []byte, err error) {
 	col := client.Database(ms.Database).Collection(ms.Collection)
 	err = col.FindOne(mongoCtx, query).Decode(&recordMap)
 
+
+	if err != nil {
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "FindOne").
+			Interface("query", query).
+			Msg("error finding document in mongo")
+
+			return
+	}
+
 	record, err = json.Marshal(recordMap)
+
+	mongoLogger.Info().
+		Str("operation", "FindOne").
+		Interface("query", query).
+		Bytes("record", record).
+		Msg("found one success")
 
 	return
 }
@@ -78,6 +128,12 @@ func (ms MongoServer) FindMany(query bson.D, results interface{}) (err error) {
 	defer cancel()
 
 	if err != nil {
+		// log error for failing to connect
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "FindMany").
+			Msg("failed connection to mongo")
+
 		return
 	}
 
@@ -85,10 +141,33 @@ func (ms MongoServer) FindMany(query bson.D, results interface{}) (err error) {
 	cur, err := col.Find(mongoCtx, query)
 
 	if err != nil {
+
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "FindMany").
+			Interface("query", query).
+			Msg("failed to execute query")
+
 		return
 	}
 
 	err = cur.All(mongoCtx, &results)
+
+	if err != nil {
+
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "FindMany").
+			Interface("query", query).
+			Msg("failed to unmarshal results from query cursor")
+
+		return
+	}
+
+	mongoLogger.Info().
+		Str("operation", "FindMany").
+		Interface("query", query).
+		Msg("success")
 
 	return
 
@@ -101,11 +180,34 @@ func (ms MongoServer) DeleteOne(query bson.D) (record map[string]interface{}, er
 	defer cancel()
 
 	if err != nil {
+		// log error for failing to connect
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "DeleteOne").
+			Msg("failed connection to mongo")
+
 		return
 	}
 
 	col := client.Database(ms.Database).Collection(ms.Collection)
 	err = col.FindOneAndDelete(mongoCtx, query).Decode(&record)
+
+	if err != nil {
+
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "DeleteOne").
+			Interface("query", query).
+			Msg("error deleting document from mongo")
+
+		return
+	}
+
+	mongoLogger.Info().
+		Str("operation", "DeleteOne").
+		Interface("query", query).
+		Interface("record", record).
+		Msg("successfully deleted document from mongo")
 
 	return
 }
@@ -117,6 +219,12 @@ func (ms MongoServer) UpdateOne(query bson.D, update []byte) (record []byte, err
 	defer cancel()
 
 	if err != nil {
+		// log error for failing to connect
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "UpdateOne").
+			Msg("failed connection to mongo")
+
 		return
 	}
 
@@ -124,6 +232,14 @@ func (ms MongoServer) UpdateOne(query bson.D, update []byte) (record []byte, err
 	nested, err := nestedUpdate(update)
 	if err != nil {
 		err = fmt.Errorf(`{"message": "Failed to Convert to Nested Dot Format", "error": "%s"}`, err.Error())
+
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "UpdateOne").
+			Interface("query", query).
+			Bytes("update", update).
+			Msg("failed to convert update to dot notation")
+
 		return
 	}
 
@@ -137,10 +253,27 @@ func (ms MongoServer) UpdateOne(query bson.D, update []byte) (record []byte, err
 
 	if err != nil {
 		err = fmt.Errorf(`{"message": "Mongo Update Operation Failed", "error": "%s"}`, err.Error())
+
+		mongoLogger.Error().
+			Err(err).
+			Str("operation", "UpdateOne").
+			Interface("query", query).
+			Bytes("update", update).
+			Msg("failed UpdateOne operation in mongo")
+
+		return
 	}
+
 	record, err = json.Marshal(rec)
 
-	log.Printf(`{ "update": "%s", "bson": "%s"}` , string(nested), string(record) )
+
+	mongoLogger.Info().
+		Str("operation", "UpdateOne").
+		Interface("query", query).
+		Bytes("update", update).
+		Bytes("nested", nested).
+		Interface("record", record).
+		Msg("failed UpdateOne operation in mongo")
 
 	return
 }
